b9d731a31e2f2108c3cfea3ab89c2d2120998cda
Dominic Serrano <dmsrojo@gmail.com>
2016-08-20 11:41:57 -0500

    App is set up and ready for editing. It is compiling sass and everything is installed

--- app/app.js
+++ app/app.js
@@ -5,36 +5,12 @@ var app = angular.module("Pinterest", ['ngRoute'])
 
 app.config(function($routeProvider, FBCreds) {
 
-  let authConfig = {
-    apiKey: FBCreds.apiKey,
-    authDomain: FBCreds.authDomain,
-    databaseURL: FBCreds.databaseURL,
-    storageBucket: FBCreds.storageBucket
-  };
-
-  firebase.initializeApp(authConfig);
 
   $routeProvider.
-  when('/login', {
-    templateUrl: 'partials/login.html',
-    controller: 'LoginCtrl'
+  when('/home', {
+    templateUrl: '',
+    controller: ''
   }).
-  when('/boards', {
-    templateUrl: 'partials/board-view.html',
-    controller: 'BoardCtrl'
-  }).
-  when('/boards/new', {
-    templateUrl: 'partials/new-board.html',
-    controller: 'NewBoardCtrl'
-  }).
-  when('/boards/:boardId', {
-    templateUrl: 'partials/pin-view.html',
-    controller: 'PinCtrl'
-  }).
-  when('/boards/pins/new/:boardId', {
-    templateUrl: 'partials/new-pin.html',
-    controller: 'NewPinCtrl'
-  }).
-  otherwise('/boards');
+  otherwise('/home');
 
 });
