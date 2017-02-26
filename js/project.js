77bbeaff9858eb248fc048c2356a06ae496e8635
Dominic Serrano <dmsrojo@gmail.com>
2017-02-26 12:02:53 -0600

    Base styles for portfolio done. Ready to javscriptify

--- js/project.js
+++ js/project.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Helper function to create DOM nodes
+const createEl = (tag) => document.createElement(tag);
+
 // Load project html
 $.ajax('./project.json')
 .then((res) => res.list)
@@ -8,29 +11,46 @@ $.ajax('./project.json')
 
 
 const displayProjects = (list) => {
-  console.log(list);
+  let d = createEl('div');
+  d.setAttribute('class', 'project-div');
+
+  let h1 = createEl('h1');
+
+  let a1 = createEl('a');
+
+  let a2 = createEl('a');
+
+  // let
+
+
+  console.log(d);
   let html = '';
 
   list.forEach(each => {
 
     html += `
     <div class="project-div">
-      <h1>${each.title}</h1>
-        <div>
-          <p>Project summary: ${each.summary}</p>
+      <div class="project-info">
+        <h1>${each.title}</h1>
+        <p>Project summary: ${each.summary}</p>
 
-          <a class="link" href="${each.githubRepo}">
-          <p>Click to View the Github Repo</p></a>
+        <a class="link" href="${each.githubRepo}">
+        Click to View the Github Repo</a>
 
-          <a class="link" href="${each.hostedUrl}">
-          <p>Click to View the hosted url</p></a>
+        <a class="link" href="${each.hostedUrl}">
+        Click to View the hosted url</a>
 
-          <p>Technologies used:</p>
-          <div class="panel-icon-list" >
-            <img src="${each.techPics[0].img}" />
-          </div>
-          <img class="project-pic" src="${each.projectPics[0]}" >
+        <p>Technologies used:<p>
+        <div class="panel-icon-list">
+          <img src="${each.techPics[0].img}" />
+        </div>
       </div>
+
+      <div class="project-pic-div">
+        <img class="project-pic" src="${each.projectPics[0]}" >
+      </div>
+
+    </div>
     `;
 
   });
